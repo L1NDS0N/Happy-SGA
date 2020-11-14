0f27a460caedcef52df373f4e83e171d7445fff0
Lindson França <l1nds0n@hotmail.com>
2020-11-13 22:30:25 -0300

    correção do erro

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -5,7 +5,7 @@ const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
 
   return response.status(418).json({
     message:
-      "O servidor diz que é impossível coar café em um bule de chá, entre em contato através das mídias do desenvolvedor https://github.com/l1nds0n",
+      "O servidor se recusa a coar café em um bule de chá, entre em contato através das mídias do desenvolvedor https://github.com/l1nds0n para obter suporte",
   });
 };
 
